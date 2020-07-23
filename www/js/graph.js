b72d0937e086dc991f29d1777ba9eeab3a6ccb9f
rob <rob@dijital.ca>
2020-07-23 18:32:59 +0000

    Tweaks.

--- www/js/graph.js
+++ www/js/graph.js
@@ -1,7 +1,7 @@
 (function() {
 	
-	var hydro = pkg('ca.dijital.hydro');
-	var anim = pkg('ca.dijital.anim');
+	let hydro = pkg('ca.dijital.hydro');
+	let anim = pkg('ca.dijital.anim');
 	
 	/**
 	 * Format a short date.
@@ -23,14 +23,14 @@
 	 * Compute the limits and ranges of a 2D data set.
 	 */
 	function computeParams(data, xfield, yfield) {
-		var values = [];
-		var minx = Infinity;
-		var maxx = -Infinity;
-		var miny = Infinity;
-		var maxy = -Infinity;
-		for(var i=0;i<data.length;++i) {
-			var x = data[i][xfield];
-			var y = data[i][yfield];
+		let values = [];
+		let minx = Infinity;
+		let maxx = -Infinity;
+		let miny = Infinity;
+		let maxy = -Infinity;
+		for(let i=0;i<data.length;++i) {
+			let x = data[i][xfield];
+			let y = data[i][yfield];
 			if(y == -9999.0)continue;
 			if(x < minx) minx = x;
 			if(y < miny) miny = y;
@@ -38,14 +38,14 @@
 			if(y > maxy) maxy = y;
 			values.push([x, y]);
 		}
-		var rangex = maxx - minx;
-		var rangey = maxy - miny;
-		var aminx = (minx - rangex * 0.1);
-		var amaxx = (maxx + rangex * 0.1);
-		var aminy = (miny - rangey * 0.1);
-		var amaxy = (maxy + rangey * 0.1);
-		var arangex = amaxx - aminx;
-		var arangey = amaxy - aminy;
+		let rangex = maxx - minx;
+		let rangey = maxy - miny;
+		let aminx = (minx - rangex * 0.1);
+		let amaxx = (maxx + rangex * 0.1);
+		let aminy = (miny - rangey * 0.1);
+		let amaxy = (maxy + rangey * 0.1);
+		let arangex = amaxx - aminx;
+		let arangey = amaxy - aminy;
 		return {
 			minx:minx, maxx:maxx, miny:miny, maxy:maxy, 
 			aminx:aminx, amaxx:amaxx, aminy:aminy, amaxy:amaxy,
@@ -75,9 +75,9 @@
 					bottom : 25,
 					top : 20
 				};
-				var tt = this._tt = document.createElement('div');
+				let tt = this._tt = document.createElement('div');
 				tt.className = 'tooltip';
-				var ttAnim = this._ttAnim = new anim.Anim(tt);
+				let ttAnim = this._ttAnim = new anim.Anim(tt);
 				ttAnim.duration = 250;
 				this._ttShow = false;
 				document.body.appendChild(tt);
@@ -91,12 +91,12 @@
 		_mouseMove : {
 			value : function(evt) {
 				if(!this._over || !this._params) return;
-				var params = this._params;
-				var tt = this._tt;
-				var box = this._box;
-				var canvas = this._canvas;
-				var x = Math.max(0, Math.min(1, (evt.offsetX - box.left) / (canvas.width - box.left - box.right)));
-				var y = Math.max(0, Math.min(1, 1 - (evt.offsetY - box.top) / (canvas.height - box.bottom - box.top)));
+				let params = this._params;
+				let tt = this._tt;
+				let box = this._box;
+				let canvas = this._canvas;
+				let x = Math.max(0, Math.min(1, (evt.offsetX - box.left) / (canvas.width - box.left - box.right)));
+				let y = Math.max(0, Math.min(1, 1 - (evt.offsetY - box.top) / (canvas.height - box.bottom - box.top)));
 				x = params.aminx + params.arangex * x;
 				y = params.aminy + params.arangey * y;
 				tt.textContent = shortDate(new Date(x)) + ' - ' + y.toFixed(2) + 'm';
@@ -139,15 +139,15 @@
 				// Sort on time.
 				data.sort(function(a, b) { return a.timestamp - b.timestamp; });
 				// Compute the ranges.
-				var params = this._params = computeParams(data, xfield, yfield);
-				var ctx = this._canvas.getContext('2d');
-				var width = this._canvas.width;
-				var height = this._canvas.height;
+				let params = this._params = computeParams(data, xfield, yfield);
+				let ctx = this._canvas.getContext('2d');
+				let width = this._canvas.width;
+				let height = this._canvas.height;
 				// Padding
-				var left = this._box.left;
-				var right = this._box.right;
-				var bottom = this._box.bottom;
-				var top = this._box.top;
+				let left = this._box.left;
+				let right = this._box.right;
+				let bottom = this._box.bottom;
+				let top = this._box.top;
 				// Clear canvas.
 				ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
 				// Draw axes.
@@ -159,28 +159,41 @@
 				ctx.moveTo(left, height - bottom);
 				ctx.lineTo(left, top);
 				// Draw y axis ticks and labels.
-				for(var i = 0, j = 0; i <= params.arangey; i += params.arangey / 10, ++j ) {
+				let step;
+				if(params.arangey > 0) {
+					step = params.arangey / 10.0;
+				} else {
+					step = 0.001;
+				}
+				for(let i = 0, j = 0; i <= params.arangey; i += step, ++j ) {
 					ctx.moveTo(left, height - bottom - i * (height - top - bottom) / params.arangey);
 					ctx.lineTo(left - 5, height - bottom - i * (height - top - bottom) / params.arangey);
 					ctx.fillText((params.aminy + j * params.arangey / 10.0).toFixed(3) + 'm', 0, height - bottom - i * (height - top - bottom) / params.arangey + 2);
 				}
+				
 				// Draw x axis ticks and labels.
-				for(var i = 0, j = 0;i <= params.arangex; i += params.arangex / 5, ++j) {
+				if(params.arangex > 0) {
+					step = params.arangex / 5;
+				} else {
+					step = 0.001;
+				}
+				for(let i = 0, j = 0;i <= params.arangex; i += params.arangex / 5, ++j) {
 					ctx.moveTo(left + i * (width - left - right) / params.arangex, height - bottom);
 					ctx.lineTo(left + i * (width - left - right) / params.arangex, height - bottom + 5);
-					var dt = new Date((params.aminx + j * params.arangex / 5.0));
-					var dtxt = shortDate(dt); 
-					var mtxt = ctx.measureText(dtxt);
+					let dt = new Date((params.aminx + j * params.arangex / 5.0));
+					let dtxt = shortDate(dt); 
+					let mtxt = ctx.measureText(dtxt);
 					ctx.fillText(dtxt, left + i * (width - left - right) / params.arangex - mtxt.width / 2, height - bottom + 15 + ((j % 2 == 1) ? 10 : 0));
 				}
+				
 				ctx.stroke();
 				// Draw lines.
 				ctx.fillStyle = '#fff';
 				ctx.beginPath();
-				var first = true;
+				let first = true;
 				params.values.forEach(function(value) {
-					var px = left + (value[0] - params.aminx) / params.arangex * (width - left - right);
-					var py = top + (value[1] - params.aminy) / params.arangey * (height - top - bottom);
+					let px = left + (value[0] - params.aminx) / params.arangex * (width - left - right);
+					let py = top + (value[1] - params.aminy) / params.arangey * (height - top - bottom);
 					if(first) {
 						ctx.moveTo(px, py);
 						first = false;
@@ -191,8 +204,8 @@
 				ctx.stroke();
 				// Draw disks.
 				params.values.forEach(function(value) {
-					var px = left + (value[0] - params.aminx) / params.arangex * (width - left - right);
-					var py = top + (value[1] - params.aminy) / params.arangey * (height - top - bottom);
+					let px = left + (value[0] - params.aminx) / params.arangex * (width - left - right);
+					let py = top + (value[1] - params.aminy) / params.arangey * (height - top - bottom);
 					ctx.beginPath();
 					ctx.moveTo(px, py);
 					ctx.arc(px, py, 3, 0, 2*Math.PI);
